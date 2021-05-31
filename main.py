108d2d510752b8d05f8d8db3a3a60533b70ea7dc
Lateef Adekunle <>
2021-05-31 23:39:42 +0000

    Added clear function

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 #Step 5
 
+from replit import clear
 import random
 import hangman_words
 from hangman_art import logo, stages
 
 print(logo)
-#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-#Delete this line: word_list = ["ardvark", "baboon", "camel"]
+
 chosen_word = random.choice(hangman_words.word_list)
 word_length = len(chosen_word)
 
 end_of_game = False
 lives = 6
 
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -25,8 +24,8 @@ for _ in range(word_length):
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+    clear()
 
-    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in display:
       print(f'You\'ve already guessed {guess}')
     #Check guessed letter
@@ -38,7 +37,6 @@ while not end_of_game:
 
     #Check if user is wrong.
     if guess not in chosen_word:
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         print(f"You guessed {guess}, that's not in the word. You lose a life")
         lives -= 1
         if lives == 0:
@@ -53,5 +51,4 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
     print(stages[lives])
